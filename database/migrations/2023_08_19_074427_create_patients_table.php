25b2a66e1310a815c8669b00e72dbdab60f64d5a
Asaba William <asabawilliamdk@yahoo.com>
2023-08-20 23:48:13 +0300

    Add birth date and age

--- database/migrations/2023_08_19_074427_create_patients_table.php
+++ database/migrations/2023_08_19_074427_create_patients_table.php
@@ -17,7 +17,9 @@ return new class extends Migration
             $table->string('patient_number');
             $table->string('full_name');
             $table->string('gender');
+            $table->string('birth_date')->nullable();
             $table->date('date_of_birth')->nullable();
+            $table->integer('age')->nullable();
             $table->string('phone_number')->nullable();
             $table->text('email')->nullable();
             $table->text('residence')->nullable();
