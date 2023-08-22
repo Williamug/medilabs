fcc5ef9021f297507a9a7c89f2d1c5a243f2093d
Asaba William <asabawilliamdk@yahoo.com>
2023-08-22 08:06:48 +0300

    Update TestResult model

--- app/Models/TestResult.php
+++ app/Models/TestResult.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -11,8 +12,8 @@ class TestResult extends Model
 {
     use HasFactory, SoftDeletes;
 
-    public function patients(): BelongsToMany
+    public function patient(): BelongsTo
     {
-        return $this->belongsToMany(Patient::class);
+        return $this->belongsTo(Patient::class);
     }
 }
