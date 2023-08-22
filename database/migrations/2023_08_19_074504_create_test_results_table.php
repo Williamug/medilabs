bb3d77d28515adef4fdfc6adb73632080705a155
Asaba William <asabawilliamdk@yahoo.com>
2023-08-22 08:09:08 +0300

    Modify test_results table

--- database/migrations/2023_08_19_074504_create_test_results_table.php
+++ database/migrations/2023_08_19_074504_create_test_results_table.php
@@ -14,12 +14,24 @@ return new class extends Migration
         Schema::create('test_results', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->nullable();
+            $table->foreignId('patient_id')->nullable();
+            $table->foreignId('visit_id')->nullable();
             $table->foreignId('lab_service_id')->nullable();
             $table->text('spacemen_id')->nullable();
             $table->foreignId('result_option_id')->nullable();
             $table->string('test_identity')->nullable();
             $table->string('result_status')->nullable();
             $table->text('comment')->nullable();
+            $table->date('visit_date')->nullable();
+            $table->integer('patient_age')->nullable();
+            $table->integer('temperature')->nullable();
+            $table->integer('weight')->nullable();
+            $table->integer('height')->nullable();
+            $table->string('next_of_kin_name')->nullable();
+            $table->string('next_of_kin_gender')->nullable();
+            $table->string('relation_to_patient')->nullable();
+            $table->string('next_of_kin_phone_number')->nullable();
+            $table->string('next_of_kin_residence')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
