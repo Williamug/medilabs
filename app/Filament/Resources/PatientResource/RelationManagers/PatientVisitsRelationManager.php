666b9020b8f4b2c01473af6c804b0a9edb5b162b
Asaba William <asabawilliamdk@yahoo.com>
2023-08-22 15:17:31 +0300

    Add relation manager

---
+++ app/Filament/Resources/PatientResource/RelationManagers/PatientVisitsRelationManager.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Filament\Resources\PatientResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PatientVisitsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'patient_visits';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('temperature')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('temperature')
+            ->columns([
+                Tables\Columns\TextColumn::make('temperature'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+}
